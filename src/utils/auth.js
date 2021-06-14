e9b5aa8206b8908d798df97075c8fc7c43b4f92f
Sergey <szemelkin@gmail.com>
2021-06-14 06:00:38 +0300

    140621 v1

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.zmovies.nomoredomains.icu';
+export const BASE_URL = 'https://api.zmovies.nomoredomains.icu';
 
 const responseCheck = (response) => response.ok ? response : Promise.reject(`Ошибка ${response.status}`);
 
@@ -39,16 +39,3 @@ export const authorize = (email, password) => {
     }) 
 };
 
-export const getContent = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    //   'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGMwMWViNzIwOGE2MDZlNmQ2YTJjMTciLCJpYXQiOjE2MjMyMTM2MzB9.VkUpI9O9bf1xxBBlN5g20qoNWYG7a6boORZNuecrxDo`,
-    }
-  })
-    .then(responseCheck)
-
-}
